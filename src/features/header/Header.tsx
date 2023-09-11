98c62ee1203aa2272b9686090cbe53b101280205
BenGregory23 <bengregorygbr@gmail.com>
2023-09-12 01:57:34 +0200

    added motion

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,6 +1,9 @@
 import {Image} from "@nextui-org/react";
 import {User} from "@nextui-org/react";
 import {Popover, PopoverTrigger, PopoverContent, Link,Card, CardBody, CardFooter,Avatar, CardHeader} from "@nextui-org/react";
+import { LayoutGroupContext, motion } from "framer-motion";
+
+
 
 
 export const Header = () => {
@@ -10,14 +13,35 @@ export const Header = () => {
         // width 100% of parent
         <div className="flex flex-row justify-between items-center p-6 md:px-12 w-full ">
             <div className="flex flex-row align-middle items-center">
+           
+            
+           
+
+            
+
+            <motion.ul
+             className=""
+             initial={{ scale: 0 }}
+             animate={{ rotate: 360, scale: 1 }}
+           
+             transition={{
+               type: "spring",
+               stiffness: 560,
+               damping: 100
+             }}
+            >
+
             <Image
             width={50}
             alt="NextUI hero Image"
             src="https://img.icons8.com/ios-glyphs/90/radar.png"
             />
+            </motion.ul>
+            <h1 className="text-4xl font-light text-gray-900"> Radar </h1>
+           
             
 
-            <h1 className="text-4xl font-light text-gray-900"> Radar </h1>
+         
             </div>
            
 
@@ -30,7 +54,7 @@ export const Header = () => {
                 size: "md",
                 color: "success",
                 isBordered: true,
-                src: "/src/assets/me.jpg",
+                src: "/public/assets/me.jpg",
             }}
             name="Ben Gregory"
            />
@@ -39,7 +63,7 @@ export const Header = () => {
       <Card shadow="none" className="max-w-[300px] border-none bg-transparent">
       <CardHeader className="justify-between">
         <div className="flex gap-3">
-          <Avatar isBordered radius="full" size="md" src="/src/assets/me.jpg" />
+          <Avatar isBordered color="success" radius="full" size="md" src="/public/assets/me.jpg" />
           <div className="flex flex-col items-start justify-center">
             <h4 className="text-small font-semibold leading-none text-default-600">Ben Gregory</h4>
             <h5 className="text-small tracking-tight text-default-500">@bengrgory</h5>
