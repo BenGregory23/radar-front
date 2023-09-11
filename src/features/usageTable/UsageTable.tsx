204537928f3681a704d1d1bb3bcedce38bc461f2
BenGregory23 <bengregorygbr@gmail.com>
2023-09-12 01:57:26 +0200

    removed unused useState

--- src/features/usageTable/UsageTable.tsx
+++ src/features/usageTable/UsageTable.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue, Spinner} from "@nextui-org/react";
 import { useAppSelector, useAppDispatch } from "../../app/hooks"
-import { selectUsage, setUsage, fetchUsage } from "./usageTableSlice"
+import { selectUsage } from "./usageTableSlice"
 import {useAsyncList} from "@react-stately/data";
 
 
@@ -26,34 +26,17 @@ const columns = [
 ];
 
 export default function UsageTable() {
-  const [data, setData] = React.useState([]);
-  const [loaded, setLoaded] = React.useState(false);
+  
 
   const dispatch = useAppDispatch()
   const usage = useAppSelector(selectUsage)
 
-  useEffect(() => {
 
-    // if user is on mobile hide the table
-    // @ts-ignore
-    if (window.innerWidth < 500) {
-      return
-    }
-
-    if(!loaded) {
-
-    // the dispatch has already been done in app so we can just use the selector
-    // @ts-ignore
-    setData(usage)
-    setLoaded(false)
-    return
-    }
-  }, [loaded, usage]);
 
   let list = useAsyncList({
     async load() {
       let res = usage
-      setLoaded(true)
+      
 
       return {
         items: res,
@@ -83,15 +66,16 @@ export default function UsageTable() {
   return (
     <Table aria-label="Example table with dynamic content" 
     sortDescriptor={list.sortDescriptor}
+    
     onSortChange={list.sort}
-    className="">
+    >
     <TableHeader columns={columns}>
       {(column) => <TableColumn allowsSorting>{column.label}</TableColumn>}
     </TableHeader>
-    <TableBody items={list.items}>
+    <TableBody items={list.items} className="dark">
       {(item) => (
         <TableRow key={item.time + item.source + item.IP + Math.random()}>
-          {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+          {(columnKey) => <TableCell >{getKeyValue(item, columnKey)}</TableCell>}
         </TableRow>
       )}
     </TableBody>
